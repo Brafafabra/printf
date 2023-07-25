28c6acf1ca0b42a450f088287e9bc4a58a5cb6a4
Brafafabra <fnkyungu@gmail.com>
2023-07-25 10:00:16 -0700

    gf

--- _printf.c
+++ _printf.c
@@ -2,114 +2,102 @@
 #include "main.h"
 
 /**
- * _printf - Produces output according to a format.
- * @format: A character string containing zero or more directives.
- *
- * Return: The number of characters printed (excluding the null byte used
- * to end output to strings).
+ * _printf - produces output according to a format
+ * @format: character string
+ * Return: number of characters printed excluding null byte
  */
 int _printf(const char *format, ...)
 {
-	int chars_printed = 0;
-	int temp;
-	int divisor;
-	int i;
-	va_list args;
-	va_start(args, format);
+    int printed = 0;
+    int temp;
+    int divisor;
+    int i;
+    va_list args;
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++; /* Move past the '%' */
-			if (*format == '\0') /* Check for the end of the format string */
-				break;
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '\0')
+                break;
 
-			/* Handle the conversion specifier */
-			switch (*format)
-			{
-				case 'c':
-					/* Fetch the next argument of type char and print it */
-					_putchar(va_arg(args, int));
-					chars_printed++;
-					break;
+            switch (*format)
+            {
+                case 'c':
+                    _putchar(va_arg(args, int));
+                    printed++;
+                    break;
 
-				case 's':
-				{
-					/* Fetch the next argument of type char* (string) and print it */
-					const char *str = va_arg(args, const char *);
-					while (*str)
-					{
-						_putchar(*str);
-						str++;
-						chars_printed++;
-					}
-					break;
-				}
+                case 's':
+                {
+                    const char *str = va_arg(args, const char *);
+                    while (*str)
+                    {
+                        _putchar(*str);
+                        str++;
+                        printed++;
+                    }
+                    break;
+                }
 
-				case 'd':
-				case 'i':
-				{
-					/* Fetch the next argument of type int and print it */
-					int num = va_arg(args, int);
-					int num_chars = 0;
+                case 'd':
+                case 'i':
+                {
+                    int num = va_arg(args, int);
+                    int num_chars = 0;
 
-					/* Handle negative numbers */
-					if (num < 0)
-					{
-						_putchar('-');
-						chars_printed++;
-						num = -num;
-					}
+                    if (num < 0)
+                    {
+                        _putchar('-');
+                        printed++;
+                        num = -num;
+                    }
 
-					/* Calculate the number of digits in the integer */
-					temp = num;
-					do
-					{
-						temp /= 10;
-						num_chars++;
-					} while (temp);
+                    temp = num;
+                    do
+                    {
+                        temp /= 10;
+                        num_chars++;
+                    } while (temp);
 
-					/* Print each digit in reverse order */
-					divisor = 1;
-					for (i = 1; i < num_chars; i++)
-						divisor *= 10;
+                    divisor = 1;
+                    for (i = 1; i < num_chars; i++)
+                        divisor *= 10;
 
-					while (divisor)
-					{
-						int digit = num / divisor;
-						_putchar(digit + '0');
-						chars_printed++;
-						num %= divisor;
-						divisor /= 10;
-					}
+                    while (divisor)
+                    {
+                        int digit = num / divisor;
+                        _putchar(digit + '0');
+                        printed++;
+                        num %= divisor;
+                        divisor /= 10;
+                    }
 
-					_putchar(num + '0');
-					chars_printed++;
-					break;
-				}
+                    _putchar(num + '0');
+                    printed++;
+                    break;
+                }
 
-				case '%':
-					/* Handle the %% case (print a single '%') */
-					_putchar('%');
-					chars_printed++;
-					break;
+                case '%':
+                    _putchar('%');
+                    printed++;
+                    break;
 
-				default:
-					/* If the conversion specifier is not recognized, ignore it */
-					break;
-			}
-		}
-		else
-		{
-			/* Regular character, just print it */
-			_putchar(*format);
-			chars_printed++;
-		}
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            _putchar(*format);
+            printed++;
+        }
 
-		format++; /* Move to the next character in the format string */
-	}
+        format++;
+    }
 
-	va_end(args);
-	return chars_printed;
+    va_end(args);
+    return (printed);
 }
