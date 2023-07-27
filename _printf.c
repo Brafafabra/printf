e04d736e2007624bf8715293013594902f009104
Brafafabra <fnkyungu@gmail.com>
2023-07-27 11:26:36 -0700

    main

--- _printf.c
+++ _printf.c
@@ -1,16 +1,6 @@
 #include <stdarg.h>
 #include <unistd.h>
-
-/**
- * _putchar - Writes a character to stdout
- * @c: The character to print
- *
- * Return: 1 on success, -1 on error.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
+#include "main.h"
 
 /**
  * _printf - Produces output according to a format
